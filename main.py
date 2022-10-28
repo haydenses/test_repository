a03b3e4f9faddb46af3eadf9cb2abadb1c029d68
haydenses <hayden.heninger@me.com>
2022-10-28 02:50:01 -0400

    added a subtract feature to the file where you can now subtract two numbers from each other

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ input1 = int(input("Enter the first number to add: "))  # gets the first user in
 input2 = int(input("Enter the second number to add: "))  # gets the second user input to add to the first number
 print(input1 + input2)  # adds the two numbers and prints the result
 
+input1 = int(input("Enter the first number to subtract: "))  # gets the first user input for the first num to subtract
+input2 = int(input("Enter the second number to subtract: "))  # gets the second input to subtract from the first input
+print(input1 - input2)  # subtracts the two numbers from each other
+
