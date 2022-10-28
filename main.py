b91749affe59830afe5014bc5b9e0446af64cf28
haydenses <hayden.heninger@me.com>
2022-10-28 02:45:18 -0400

    added functionality to main to make it able to add two numbers together and print it to the console

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-input1 = int(input("Enter the first number to add: ")) # gets the first user input for a number to add
-input2 = int(input("Enter the second number to add: ")) # gets the second user input to add to the first number
-print(input1 + input2) # adds the two numbers and prints the result
+input1 = int(input("Enter the first number to add: "))  # gets the first user input for a number to add
+input2 = int(input("Enter the second number to add: "))  # gets the second user input to add to the first number
+print(input1 + input2)  # adds the two numbers and prints the result
 
